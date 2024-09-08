752b0332f6a79ef0581d915c505f888d96204be5
erfanscode <codeerfan@gmail.com>
2024-09-08 04:28:59 +0330

    add admin display for admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+# blog models
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ['id', 'title', 'author', 'publish', 'status']
+  list_filter = ['author', 'publish', 'status']
+  search_fields = ['title', 'description', 'author']
+  list_editable = ['status']
+  date_hierarchy = 'publish'
+  raw_id_fields = ['author']
+  prepopulated_fields = {'slug': ['title']}
