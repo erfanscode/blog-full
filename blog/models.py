c786ea036688697fef39388068bb95fd9ef08cdd
erfanscode <codeerfan@gmail.com>
2024-09-10 16:21:33 +0330

    add published manager for post model

--- blog/models.py
+++ blog/models.py
@@ -3,31 +3,39 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+# Managers
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 # Post Model
 class Post(models.Model):
-  class Status(models.TextChoices):
-    DRAFT     = 'DR', 'Draft'
-    PUBLISHED = 'PB', 'Published'
-    REJECTED  = 'RJ', 'Rejected'
+    class Status(models.TextChoices):
+        DRAFT = 'DR', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        REJECTED = 'RJ', 'Rejected'
 
-  # relations
-  author      = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
-  # data fields
-  title       = models.CharField(max_length=250)
-  description = models.TextField()
-  slug        = models.SlugField(max_length=250)
-  #date
-  publish = models.DateTimeField(default=timezone.now)
-  create  = models.DateTimeField(auto_now_add=True)
-  update  = models.DateTimeField(auto_now=True)
-  # choice fields
-  status  = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    # relations
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_posts')
+    # data fields
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(max_length=250)
+    #date
+    publish = models.DateTimeField(default=timezone.now)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    # choice fields
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    # call managers
+    objects = models.Manager()
+    published = PublishedManager()
 
-  class Meta:
-    ordering = ['-publish']
-    indexes = [
-      models.Index(fields=['-publish'])
-    ]
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
