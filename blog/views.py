f76ca90c051caba698ec4140b566e0d8471fc8f5
erfanscode <codeerfan@gmail.com>
2024-09-11 04:40:44 +0330

    change view from test to initial view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,26 @@
 # View for blog
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
+    # This view for home page
     return HttpResponse("Home Page")
 
 def post_list(request):
-    return HttpResponse("Posts")
+    # This view for showing all posts
+    posts = Post.published.all()
+    context = {
+        'posts': posts
+    }
+    render(request, "template.html", context)
 
 def post_detail(request, id):
-    return HttpResponse(f"Post: {id}")
+    # This view for showing detail post
+    post = Post.published.get(id=id)
+    context = {
+        'post': post
+    }
+    render(request, "template2.html", context)
