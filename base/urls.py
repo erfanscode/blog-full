a5bf9b616c2492916df715a73d33ba55808842bc
erfanscode <codeerfan@gmail.com>
2024-09-11 04:16:54 +0330

    add initial urls for blog app

--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # admin url
     path('admin/', admin.site.urls),
+    # blog url
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
