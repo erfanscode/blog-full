2025a8d51ab1f2b70196f829704341b6612a192e
erfanscode <codeerfan@gmail.com>
2024-09-14 20:16:46 +0330

    change views to Class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("posts/", views.post_list, name="post_list"),
-    path("posts/<int:id>", views.post_detail, name="post_detail")
+    path("posts/", views.PostListView.as_view(), name="post_list"),
+    path("posts/<pk>", views.PostDetailView.as_view(), name="post_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,37 +1,22 @@
 # View for blog
-from lib2to3.fixes.fix_input import context
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 def index(request):
     # This view for home page
     return HttpResponse("Home Page")
 
-def post_list(request):
-    # This view for showing all posts
-    posts = Post.published.all()
-    paginator = Paginator(posts, 1)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    context = {
-        'posts': posts
-    }
-    return render(request, "blog/list.html", context)
+class PostListView(ListView):
+    # class for post list
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'blog/list.html'
 
-def post_detail(request, id):
-    # This view for showing detail post
-    try:
-        post = Post.published.get(id=id)
-    except:
-        raise Http404("Post Not Found")
-    context = {
-        'post': post
-    }
-    return render(request, "blog/detail.html", context)
+
+class PostDetailView(DetailView):
+    # class for post detail
+    model = Post
+    template_name = 'blog/detail.html'
