141d3a34d466df455701b95e1e31b73f22cc366d
erfanscode <codeerfan@gmail.com>
2024-09-22 06:28:11 +0330

    change database to postgresql

--- base/settings.py
+++ base/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'erfan',
+        'PASSWORD': '123456789',
+        'PORT': '5432',
     }
 }
 
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,4 @@
 # Model for blog
-from tabnanny import verbose
-
 from django.db import models
 from django.template.defaultfilters import default
 from django_jalali.db import models as jmodels
