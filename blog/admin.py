6d5868063bbe5d7829f05d6d7fdd1fdb601942bc
erfanscode <codeerfan@gmail.com>
2024-09-25 02:48:56 +0330

    add InlineAdminModel for Image and Comment models

--- blog/admin.py
+++ blog/admin.py
@@ -8,6 +8,19 @@ admin.sites.AdminSite.site_header = 'پنل مدیریت جنگو'
 admin.sites.AdminSite.site_title = 'پنل'
 admin.sites.AdminSite.index_title = 'مدیریت وبسایت'
 
+
+# Inline model admin classes
+class ImageInline(admin.TabularInline):
+  model = Image
+  extra = 0
+
+
+class CommentInline(admin.TabularInline):
+  model = Comment
+  extra = 0
+
+
+# show models content in admin panel
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
   list_display = ['id', 'title', 'author', 'publish', 'status']
@@ -17,6 +30,7 @@ class PostAdmin(admin.ModelAdmin):
   date_hierarchy = 'publish'
   raw_id_fields = ['author']
   prepopulated_fields = {'slug': ['title']}
+  inlines = [ImageInline, CommentInline]
 
 
 @admin.register(Ticket)
