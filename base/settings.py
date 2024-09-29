53d8edfe7cc80572cbed11ee198a1feb36e0fb79
erfanscode <codeerfan@gmail.com>
2024-09-29 15:15:38 +0330

    add reset password to blog

--- base/settings.py
+++ base/settings.py
@@ -143,3 +143,7 @@ DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = False
 LOGIN_REDIRECT_URL = '/blog/profile/'
 LOGIN_URL = '/blog/login/'
 LOGOUT_URL = '/blog/logout/'
+
+
+# Send email to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- blog/urls.py
+++ blog/urls.py
@@ -20,4 +20,11 @@ urlpatterns = [
     path("logout/", auth_view.LogoutView.as_view(), name="logout"),
     path("password-change/", auth_view.PasswordChangeView.as_view(success_url='done'), name="password_change"),
     path("password-change/done", auth_view.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path("password-reset/", auth_view.PasswordResetView.as_view(success_url="done"), name="password_reset"),
+    path("password-reset/done", auth_view.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password-reset/<uidb64>/<token>/",
+         auth_view.PasswordResetConfirmView.as_view(success_url="/blog/password-reset/complete"),
+         name="password_reset_confirm"
+    ),
+    path("password-reset/complete/", auth_view.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
